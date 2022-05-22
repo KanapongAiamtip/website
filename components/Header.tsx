a1af12f60b15d1a2a6eec7c25497fcf9096cc8df
ppromxx <kannaphongi62@nu.ac.th>
2022-05-23 04:13:23 +0700

    fix favicon

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,6 +31,23 @@ const Header = (props) => {
         content="6zN_DV-uihM3ghNKdbtKIDTZXlcdQv9WFH5BHz8wnyE"
       />
       <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
       <title>{title !== null ? title : 'kannapong Aiamtip'}</title>
     </Head>
   );
