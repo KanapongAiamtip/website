1491480eb0f25f7b16afbb2449b63b1ecbcf5fc3
ppromxx <kannaphongi62@nu.ac.th>
2022-05-23 03:42:31 +0700

    fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,41 +14,24 @@ const Header = (props) => {
         name="description"
         content="Developer. Designer. Tech Enthusiast."
       />
-      <meta property="og:title" content="P'Captain-RoV Gamer website" />
+      <meta property="og:title" content="knp soda website" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://sidv.dev" />
-      <meta property="og:image" content="/assets/pp.webp" />
+      <meta property="og:url" content="" />
+      <meta property="og:image" content="" />
       <meta
         property="og:description"
-        content="Developer. Designer. Tech Enthusiast."
+        content=" I'm a software developer from Phitsanulok Thailand. 🔥"
       />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="https://sidv.dev" />
-      <meta name="twitter:creator" content="@sidv_22" />
-      <meta name="twitter:image" content="/assets/pp.webp" />
+      <meta name="twitter:site" content="" />
+      <meta name="twitter:creator" content="" />
+      <meta name="twitter:image" content="" />
       <meta
         name="google-site-verification"
         content="6zN_DV-uihM3ghNKdbtKIDTZXlcdQv9WFH5BHz8wnyE"
       />
       <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <title>{title !== null ? title : 'Siddhartha Varma'}</title>
+      <title>{title !== null ? title : 'kannapong Aiamtip'}</title>
     </Head>
   );
 };
